6fddda948d6371cce59402f0c9ca433dc07977be
rekhav55 <rekhavdas@gmail.com>
2022-01-08 12:08:18 +0530

    Deployed

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ mongoose.connect(process.env.MONGO_URI,{
     useNewUrlParser: true, useUnifiedTopology: true,
 }).then(()=>{
     console.log('Database connected...');
-})
+});
+
+const port = process.env.PORT || 3000;
 
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -19,4 +21,4 @@ app.use(express.static(__dirname+'/public'));
 app.use('/', require('./routes/index'));
 app.use('/books', require('./routes/book'));
 
-app.listen(3000, ()=>{console.log('You are listening on port 3000')});
+app.listen(port, ()=>{console.log('You are listening on port 3000')});
