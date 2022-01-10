fcca6e0dfa0dbcfd45ee84badaeb643e13816f85
rekhav55 <rekhavdas@gmail.com>
2022-01-10 15:40:08 +0530

    Added CSS

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 3000;
 
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname));
 
 app.use('/', require('./routes/index'));
 app.use('/books', require('./routes/book'));
